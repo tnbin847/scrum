1d4d631d505bafe8ed3e2dc4d4e5ad8623d5c2bc
tnqls847 <tnqls3718@gmail.com>
2024-11-17 17:38:38 +0900

    Initial commit

---
+++ src/test/java/com/sukong/scrum/ScrumApplicationTests.java
@@ -0,0 +1,13 @@
+package com.sukong.scrum;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ScrumApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
